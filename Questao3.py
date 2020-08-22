dd9f76ce272627c4e8511fa6de0a6892dc683598
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-22 09:03:49 -0300

    Create Questao3.py
    Algoritmo que identifica se o número  é  positivo, negativo  ou neutro.

---
+++ Questao3.py
@@ -0,0 +1,8 @@
+n = int(input('digite um número'))
+
+if n > 0:
+  print('o número {} é positivo.'.format(n))
+if n == 0:
+    print('o numero digitado é  neutro ')
+if n < 0:
+  print('o numero {} é negativo.'.format(n))
