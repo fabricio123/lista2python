1aa07d1c952b978f87b3d8771395fa41c892c65a
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-22 09:20:51 -0300

    Create Questao3.py
    Algoritmo que identifica um letra como sexo feminino, masculino ou sexo invalido a partir de estururas condicionais.

---
+++ Questao3.py
@@ -0,0 +1,13 @@
+
+letra = str(input('digite a letra'))
+F = 'F'
+M = 'M'
+
+if letra == 'F':
+  print('sexo feminino')
+
+if letra == 'M':
+  print('sexo masculino')
+
+if letra != F and M:
+  print('sexo inválido')
